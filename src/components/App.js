0614773eec967d84e1bf62c180230240c916b825
Elena-Gallego <elenagallego.front@gmail.com>
2020-12-11 19:21:26 +0100

    inicializated laocoonte project

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ComponenteMadre from './ComponenteMadre';
+import '../stylesheets/reset.scss';
+import '../stylesheets/App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <ComponenteMadre />
+      </div>
+    );
+  }
+}
+
+export default App;
